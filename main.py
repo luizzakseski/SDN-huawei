dd222ab76a9362f08ca2925456945a018fae3330
luizzakseski <l.zakseski@gmail.com>
2021-10-29 16:37:41 -0300

    recursive search

--- main.py
+++ main.py
@@ -8,9 +8,11 @@ import switch
 def vlan1():
     # this function verify if the switch has mac address in vlan1 and then print the output
     comm.expect(prompt)
-    comm.send("N\n")
-    comm.send("display mac-address vlan 1\n")
+    comm.send("display mac-address vlan 1")
     comm.expect(prompt)
+    output = comm.current_output_clean
+    print(prompt)
+    print("MACs = ", output.split("Total items displayed =", 1)[1])
     comm.close()
 
 
@@ -19,9 +21,10 @@ if __name__ == '__main__':
     password = config.password
     switchdata = switch.data
     for ip, prompt in switchdata:
-        system('cls')
         ssh = paramiko.SSHClient()
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         ssh.connect(ip, username=login, password=password)
-        comm = SSHClientInteraction(ssh, timeout=1, display=True)
+        comm = SSHClientInteraction(ssh, timeout=1, display=False)
         vlan1()
+    else:
+        print("loop finished")
